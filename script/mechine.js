4a296236e18a30189ec1c10dba76cb79ffc2de5a
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-19 22:35:15 +0600

    feat: mechine code

---
+++ script/mechine.js
@@ -0,0 +1,8 @@
+// machine id --> input value
+function getValueFromInput(id){
+    const input = document.getElementById(id);
+    const value = input.value;
+    console.log(id, value);
+    return value;
+
+}
